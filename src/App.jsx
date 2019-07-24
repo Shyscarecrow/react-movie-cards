183cac2d251fc77b606fcad4884849479e1ef12d
Aleksandra Ivleva <Aleksandra_Ivleva@epam.com>
2019-07-24 10:59:56 +0300

    Small fix (div -> React.Fragment)

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Header } from './components/Header';
 import { Movies } from './components/Movie/Movies';
 
 export const App = () => (
-  <div>
+  <>
     <Header title="React Movie Cards" />
     <div className="mt-5">
       <Movies />
     </div>
-  </div>
+  </>
 );
