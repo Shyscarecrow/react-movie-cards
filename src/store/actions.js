03f522170e924cb3b578417dd7e820d331680488
Aleksandra Ivleva <Aleksandra_Ivleva@epam.com>
2019-07-24 10:53:30 +0300

    Remove reducer and actions files

--- src/store/actions.js
+++
@@ -1,4 +0,0 @@
-import { createAction } from 'redux-actions';
-import { fetchMoviesListWithExtras } from '../services/movieService';
-
-export const fetchMovies = createAction('FETCH_MOVIES', fetchMoviesListWithExtras);
